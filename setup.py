1499c01818eb71644b18f39de80b19ff30acf360
Michael Hahn <mwhahn@gmail.com>
2014-12-12 13:02:11 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+import service
+
+requirements = [
+# TODO package requirements
+]
+
+setup_requirements = [
+    'nose>=1.0',
+    'coverage>=1.0',
+    'mock==1.0.1',
+]
+
+setup(
+    name='python-soa-protobuf',
+    version=service.__version__,
+    description='service layer using protobufs',
+    packages=find_packages(exclude=[
+        "*.tests",
+        "*.tests.*",
+        "tests.*",
+        "tests",
+    ]),
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+)
