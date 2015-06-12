d067924667278534611254ff659635475a2b9d10
Michael Hahn <mwhahn@gmail.com>
2015-06-12 16:39:00 -0700

    Stay consistent with the other "get_object" parameters

--- service/control.py
+++ service/control.py
@@ -164,7 +164,7 @@ class Client(object):
 
         return response_wrapper
 
-    def get_object(self, action_name, return_object, **action_kwargs):
+    def get_object(self, action, return_object, **action_kwargs):
         response = self.call_action(action_name, **action_kwargs)
         return getattr(response.result, return_object)
 
