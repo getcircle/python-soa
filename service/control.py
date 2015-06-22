7cf173eeb37ac0b81ff1879f900c25a7e09a60b4
Michael Hahn <mwhahn@gmail.com>
2015-06-21 23:43:11 -0700

    Fix undefined variable

--- service/control.py
+++ service/control.py
@@ -165,7 +165,7 @@ class Client(object):
         return response_wrapper
 
     def get_object(self, action, return_object, **action_kwargs):
-        response = self.call_action(action_name, **action_kwargs)
+        response = self.call_action(action, **action_kwargs)
         return getattr(response.result, return_object)
 
 
